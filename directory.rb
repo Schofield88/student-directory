3221e4c5c49bda7fda9e8879b0016d84fbf9c68f
Luke Phyall <lukeprospect@yohoo.com>
2019-01-25 19:26:40 +0000

    Add students.count method.

--- directory.rb
+++ directory.rb
@@ -16,16 +16,16 @@ students = [
 ]
 puts "The students of Villain Academy"
 puts "-------------------------------"
-puts students [0]
-puts students [1]
-puts students [2]
-puts students [3]
-puts students [4]
-puts students [5]
-puts students [6]
-puts students [7]
-puts students [8]
-puts students [9]
-puts students [10]
+puts students[0]
+puts students[1]
+puts students[2]
+puts students[3]
+puts students[4]
+puts students[5]
+puts students[6]
+puts students[7]
+puts students[8]
+puts students[9]
+puts students[10]
 # Print out number of students, without adding a new line
-print "Overall we have #{student_count} great students."
+print "Overall we have #{students.count} great students."
