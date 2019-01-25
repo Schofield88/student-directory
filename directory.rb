1abb05a9b0eea9531106908e32a462b343797bb0
Luke Phyall <lukeprospect@yohoo.com>
2019-01-25 19:24:26 +0000

    Add student array, puts student by index.

--- directory.rb
+++ directory.rb
@@ -1,17 +1,31 @@
 student_count = 11
 # First, add and print student names
+students = [
+
+  "Dr Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex Delarge",
+  "The Wicked Witch of the West",
+  "The Terminator",
+  "Freedy Kruger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+]
 puts "The students of Villain Academy"
 puts "-------------------------------"
-puts "Dr Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex Delarge"
-puts "The Wicked Witch of the West"
-puts "The Terminator"
-puts "Freedy Kruger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+puts students [0]
+puts students [1]
+puts students [2]
+puts students [3]
+puts students [4]
+puts students [5]
+puts students [6]
+puts students [7]
+puts students [8]
+puts students [9]
+puts students [10]
 # Print out number of students, without adding a new line
 print "Overall we have #{student_count} great students."
