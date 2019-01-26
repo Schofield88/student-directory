2be2248292c7f47fef319771942da197670c66d6
Luke Phyall <lukeprospect@yohoo.com>
2019-01-26 19:47:10 +0000

    Method added to build hash rather than hard code it.

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+=begin
 # First, add and print student names
 students = [
   {name: "Dr Hannibal Lecter", cohort: "november"},
@@ -12,20 +13,49 @@ students = [
   {name: "Joffrey Baratheon", cohort: "november"},
   {name: "Norman Bates", cohort: "november"}
 ]
+=end
 
 def print_header
+
   puts "The students of Villain Academy"
   puts "-------------------------------"
+
 end
 
 def print(students)
+
   students.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)"  }
+
 end
 
 def print_footer(names)
+
   puts "Overall we have #{names.count} great students."
+
 end
 
+def input_students
+
+  puts "Please enter the names of the students."
+  puts "Press enter twice to exit."
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while name is not empty, repeat this code
+  while !name.empty?
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students."
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of input_students
+  students
+
+end
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
