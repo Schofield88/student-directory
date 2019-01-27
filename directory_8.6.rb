c8dd56a075343616a3a2b3bd16a3c45f1d117177
Luke Phyall <lukeprospect@yohoo.com>
2019-01-27 15:56:45 +0000

    Add series of exercises to directory functionality.

---
+++ directory_8.6.rb
@@ -0,0 +1,63 @@
+=begin
+# First, add and print student names
+students = [
+  {name: "Dr Hannibal Lecter", cohort: "november"},
+  {name: "Darth Vader", cohort: "november"},
+  {name: "Nurse Ratched", cohort: "november"},
+  {name: "Michael Corleone", cohort: "november"},
+  {name: "Alex Delarge", cohort: "november"},
+  {name: "The Wicked Witch of the West", cohort: "november"},
+  {name: "The Terminator", cohort: "november"},
+  {name: "Freedy Kruger", cohort: "november"},
+  {name: "The Joker", cohort: "november"},
+  {name: "Joffrey Baratheon", cohort: "november"},
+  {name: "Norman Bates", cohort: "november"}
+]
+=end
+
+def print_header
+
+  puts "The students of Villain Academy"
+  puts "-------------------------------"
+
+end
+
+def print(students)
+
+  students.each { |element|
+    puts "#{students.index(element) + 1}: #{element[:name]} (#{element[:cohort]} cohort)"
+  }
+
+end
+
+def print_footer(names)
+
+  puts "Overall we have #{names.count} great students."
+
+end
+
+def input_students
+
+  puts "Please enter the names of the students.".center(200)
+  puts "Press enter twice to exit.".center(200)
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while name is not empty, repeat this code
+  while !name.empty?
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students."
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of input_students
+  students
+
+end
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
