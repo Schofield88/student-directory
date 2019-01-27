f025229a4df701978c4881921c9613833e680366
Luke Phyall <lukeprospect@yohoo.com>
2019-01-27 16:14:45 +0000

    Add interactive menu (currently broken).

--- directory.rb
+++ directory.rb
@@ -24,7 +24,7 @@ end
 
 def print(students)
 
-  students.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)"  }
+  students.each { |element| puts "#{element[:name]} (#{element[:cohort]} cohort)"  }
 
 end
 
@@ -55,7 +55,34 @@ def input_students
 
 end
 
+def interactive_menu
+
+  loop do
+    puts "1. Input the students."
+    puts "2. Show the list of students."
+    puts "9. Exit."
+    selection = gets.chomp
+    case selection
+      when "1"
+        students = input_students
+      when "2"
+        print_header
+        print(students)
+        print_footer(students)
+      when "9"
+        exit # termintaes the program
+      else
+        puts "Please select a valid command."
+      end
+  end
+
+end
+
+=begin
 students = input_students
 print_header
 print(students)
 print_footer(students)
+=end
+
+interactive_menu
