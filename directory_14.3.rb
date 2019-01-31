a3c90b6fefc48348333513cef73734ef9ca92e67
Luke Phyall <lukeprospect@yohoo.com>
2019-01-31 18:17:35 +0000

    First commit. General refactor.

---
+++ directory_14.3.rb
@@ -0,0 +1,129 @@
+@students = []
+@cohort = :February
+
+def print_header
+
+  puts "The students of Villain Academy"
+  puts "-------------------------------"
+
+end
+
+def print_student_list
+
+  @students.each { |element| puts "#{element[:name]} (#{element[:cohort]} cohort)"  }
+
+end
+
+def print_footer(names)
+
+  puts "Overall we have #{names.count} great students."
+
+end
+
+def input_students
+
+  puts "Please enter the names of the students."
+  puts "Press enter twice to exit."
+  name = STDIN.gets.chomp
+  while !name.empty?
+    push_to_students(name)
+    puts "Now we have #{@students.count} students."
+    name = STDIN.gets.chomp
+  end
+
+end
+
+def print_menu
+
+  puts ""
+  puts "=" * 30
+  puts "1. Input the students."
+  puts "2. Show the list of students."
+  puts "3. Save the list of students to csv."
+  puts "4. Load student directory from csv."
+  puts "9. Exit."
+  puts "=" * 30
+  puts ""
+
+end
+
+def show_students
+
+  print_header
+  print_student_list
+  print_footer(@students)
+
+end
+
+def process(selection)
+
+  case selection
+    when "1" then input_students
+    when "2" then show_students
+    when "3" then save_students
+    when "4" then load_students
+    when "9" then exit
+    else
+      puts "Please select a valid command."
+    end
+
+end
+
+def save_students
+
+  file = File.open("students.csv", "w") # open the file for writing
+  @students.each { |student| # iterate over the students
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line }
+  file.close
+  puts "Student directory saved."
+
+end
+
+def load_students(filename = "students.csv")
+
+  file = File.open(filename, "r")
+  file.readlines.each { |line|
+    name, cohort = line.chomp.split(",")
+      push_to_students(name) }
+  file.close
+  puts ("*" * 30).center(80)
+  puts "Student directory loaded.".center(80)
+  puts "Loaded #{@students.count} from #{filename}".center(80)
+  puts ("*" * 30).center(80)
+
+end
+
+def try_load_students
+
+  if ARGV.first.nil?
+    load_students
+  else
+    if File.exists?(filename)
+      load_students(filename)
+    else
+      puts "Sorry, #{filename} does not exist.".center(80)
+      exit
+    end
+  end
+
+end
+
+def push_to_students(name)
+
+  @students << {name: name, cohort: @cohort}
+
+end
+
+def interactive_menu
+
+  loop do
+    print_menu
+    process(STDIN.gets.chomp)
+  end
+
+end
+
+try_load_students
+interactive_menu
